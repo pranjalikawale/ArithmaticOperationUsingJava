72c7ca265b4773d9c95d0cddec1c7a7c1272450e
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-20 14:52:36 -1200

    ADDED: Substract two integer

--- ArithmaticExpression/src/main/java/com/bl/arithmaticexpression/Arithmatic.java
+++ ArithmaticExpression/src/main/java/com/bl/arithmaticexpression/Arithmatic.java
@@ -4,5 +4,8 @@ public class Arithmatic {
     public int add(int val1,int val2){
         return val1+val2;
     }
+    public int sub(int val1,int val2){
+        return val1-val2;
+    }
 }
 
--- ArithmaticExpression/src/test/java/com/bl/arithmaticexpression/ArithmaticTest.java
+++ ArithmaticExpression/src/test/java/com/bl/arithmaticexpression/ArithmaticTest.java
@@ -10,4 +10,10 @@ public class ArithmaticTest {
     {
         Assert.assertEquals(5,arithmatic.add(2,3));
     }
+    @Test
+    public void givenTwoInput_WhenSubstract_ShouldReturnDifference()
+    {
+        Assert.assertEquals(1,arithmatic.sub(3,2));
+    }
+
 }
